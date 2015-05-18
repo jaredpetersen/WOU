275d4c2779e296447d022e1fac3d70e44a7d590f
Jared Petersen <jpetersen11@wou.edu>
2015-05-18 14:08:50 -0700

    Added API file for metrics

---
+++ CS425-430/AndroidIndividualProject/API/ownershipfour.php
@@ -0,0 +1,52 @@
+<?php
+header('Content-Type: application/json');
+include('config.php');
+
+if (isset($_GET['key'])) {
+	$key = $_GET['key'];
+
+	if ($key == $androidauthentication) {
+		
+		// Get data from the URL
+		if (isset($_GET['user'])) {
+			$user = $_GET['user'];
+			$status = $_GET['status'];
+		}
+
+		else {
+			// Default user is just the initial
+			$user = 0;
+		}
+
+		// SQL Statement
+		$sql = "SELECT status, count(*) as gameCount FROM OwnedGames, CompletionStatus WHERE OwnedGames.completionStatusId = CompletionStatus.completionStatusId and OwnedGames.userId = $user and OwnedGames.completionStatusId = $status group by status order by status asc";
+
+		// Create connection
+		$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+		// Check connection
+		if ($conn->connect_error) {
+    			die("Connection Failed");
+		}
+
+		$result = $conn->query($sql);
+
+		// Get the data
+		while($row = $result->fetch_assoc()) {
+			$output[] = $row;
+		}
+
+		echo '{"results": ';
+
+		// Output data as a JSON
+		echo(json_encode($output));
+
+		echo '}';
+
+		$conn->close();
+	}
+}
+else {
+	echo "Access Denied";
+}
+?>
